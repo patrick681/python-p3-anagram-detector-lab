061579c284904f9451cd447d1ffa95a1bfa59cb0
patrick681 <kamaupatrick332@gmail.com>
2025-05-13 03:28:36 +0300

    .

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,23 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+        self.sorted_word = sorted(self.word)
+
+    def match(self, word_list):
+        matches = []
+        for w in word_list:
+            if w.lower() != self.word and sorted(w.lower()) == self.sorted_word:
+                matches.append(w)
+        return matches
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+        self.sorted_word = sorted(self.word)
+
+    def match(self, word_list):
+        matches = []
+        for w in word_list:
+            if w.lower() != self.word and sorted(w.lower()) == self.sorted_word:
+                matches.append(w)
+        return matches
